6cfb2d0da54b6eb76c159531a0517443b6eb7b1d
meaann <maryannambos26@gmail.com>
2023-09-28 14:06:28 +0800

    09/28/23

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -47,4 +47,11 @@ class Status(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)      
     status = models.CharField(max_length=200,null=True, choices=STATUS)
 
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
+def __str__(self):
+        return self.message
