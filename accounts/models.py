32afbdef388e49e12c1a6c3216e6ad4a1619b5d0
meaann <maryannambos26@gmail.com>
2023-10-03 14:00:36 +0800

    10/03/23

--- accounts/models.py
+++ accounts/models.py
@@ -67,3 +67,65 @@ class Item(models.Model):
     item_unit = models.CharField(max_length=50)
     item_quantity = models.IntegerField()
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+from django.db import models
+
+
+
+# Create your models here.
+
+class Requester (models.Model):
+    name = models.CharField(max_length=200,null=True)
+    email = models.CharField(max_length=200,null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self)   :
+        return self.name
+    
+class Tag (models.Model):
+    name = models.CharField(max_length=200,null=True)
+
+    def __str__(self)   :
+        return self.name
+
+
+from django.db import models
+class Products(models.Model):
+
+    CATEGORY = (
+    ('Sports', 'Sports'),
+    ('Laboratory', 'Laboratory')
+    )
+    name = models.CharField(max_length=200,null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200,null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200,null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self)   :
+        return self.name
+
+
+class Status(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Approved', 'Approved'),
+        ('Declined', 'Declined'),
+    )
+
+    requester = models.ForeignKey(Requester, null=True, on_delete=models.SET_NULL)
+    products = models.ForeignKey(Products, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True)      
+    status = models.CharField(max_length=200,null=True, choices=STATUS)
+
+
+from django.db import models
+
+class Item(models.Model):
+    item_no = models.IntegerField()
+    item_name = models.CharField(max_length=255)
+    item_description = models.TextField()
+    unit = models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
